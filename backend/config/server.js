a52e29dceef7ba16345fa1a190c27643edb17f41
Bartholomeas <veir@onet.pl>
2021-09-05 19:35:21 +0200

    initializing commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8bb918082ca9bbf34dbf0757f321e710'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e9986e07-470d-40b1-9f77-fefebe3364b2'
+};

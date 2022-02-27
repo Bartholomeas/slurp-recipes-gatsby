6427db49bcfc1d0979ce8b3803ab5ad908500c83
Bartholomeas <veir@onet.pl>
2022-02-27 16:18:07 +0100

    add: (Mongodb) database configuration with strapi

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
     "gatsby-plugin-offline",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-testing",
+    "gatsby-source-mongodb",
 
     {
       resolve: `gatsby-source-strapi`,

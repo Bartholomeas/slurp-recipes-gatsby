dcd3a933045ae65cdb3d1ebbecb499f760953ccb
Bartholomeas <veir@onet.pl>
2022-08-01 21:42:22 +0200

    filter logic isnt working as it should, trying to fix

--- frontend/src/hooks/useFilters.js
+++ frontend/src/hooks/useFilters.js
@@ -27,18 +27,55 @@ const useFilters = () => {
     const typesConditional = choosenFilters.types
     const dietsConditional = choosenFilters.diets
 
-    console.log(difficultiesConditional)
+    // const heh = recipes.reduce((result, value) => {
+    //   if (value.diets[0].diets === dietsConditional) return result
+    //   return result.push(value)
+    // }, [])
+
+    // for (const property in choosenFilters) {
+    //   console.log(choosenFilters[property])
+    // }
+    // for (const property in choosenFilters) {
+    //   let propertyType
+    //   if (property === "difficulties") {
+    //     propertyType = difficultiesConditional
+    //   } else if (property === "types") {
+    //     propertyType = typesConditional
+    //   } else if (property === "diets") {
+    //     propertyType = dietsConditional
+    //   }
+    //   if (recipe.property[0].property === propertyType) {
+    //     console.log("gitjest")
+    //   }
+    // }
     const filteredRecipesByFilters = recipes.filter(recipe => {
-      return (
-        recipe.difficulties[0].difficulties === difficultiesConditional &&
-        recipe.diets[0].diets === dietsConditional &&
-        recipe.types[0].types === typesConditional
-      )
+      for (const property in choosenFilters) {
+        let propertyType
+        if (property === "difficulties") {
+          propertyType = difficultiesConditional
+        } else if (property === "types") {
+          propertyType = typesConditional
+        } else if (property === "diets") {
+          propertyType = dietsConditional
+        }
+        console.log(property)
+      }
+
+      //   return (
+      //     (recipe.difficulties[0].difficulties === difficultiesConditional &&
+      //       recipe.diets[0].diets === dietsConditional &&
+      //       recipe.types[0].types === typesConditional) ||
+      //     (recipe.types[0].types === typesConditional &&
+      //       recipe.diets[0].diets === dietsConditional) ||
+      //     (recipe.types[0].types === typesConditional &&
+      //       recipe.difficulties[0].difficulties === difficultiesConditional) ||
+      //     (recipe.diets[0].diets === dietsConditional &&
+      //       recipe.difficulties[0].difficulties === difficultiesConditional)
+      //   )
     })
-    // console.log(dietConditional, difficultyConditional, typeConditional)
     console.log(filteredRecipesByFilters)
 
-    dispatch(recipesActions.filterRecipes(filteredRecipesByFilters))
+    // dispatch(recipesActions.filterRecipes(filteredRecipesByFilters))
   }
 
   return { getFilters, clearFilters, filterRecipes }

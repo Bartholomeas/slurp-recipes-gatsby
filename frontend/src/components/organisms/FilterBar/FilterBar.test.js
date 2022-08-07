e08c0506b2a60d6cd9393271b87ff1e2934b2ace
Bartholomeas <veir@onet.pl>
2022-08-07 16:40:52 +0200

    test: mocked data to filterbar by hand (not so flexible approach)

--- frontend/src/components/organisms/FilterBar/FilterBar.test.js
+++ frontend/src/components/organisms/FilterBar/FilterBar.test.js
@@ -2,19 +2,16 @@ import React from "react"
 import {render, screen, fireEvent} from "@testing-library/react"
 import FilterBar from "./FilterBar"
 import TestWrapper from "../../providers/TestWrapper"
+import filterBar from "./FilterBar";
 
 
 
 beforeEach(() => {
-    StaticQuery.mockImplementationOnce(({ render }) =>
-        render({
-            site: {
-                siteMetadata: {
-                    title: `Default Starter`,
-                },
-            },
-        })
-    )
+    const filterbarData =  {
+        allStrapiDiets: {nodes:[{diets: "wegańska"}, {diets:"wege"}, {diets:"bez-laktozy"}, {diets: "zbilansowana"}]},
+        allStrapiDifficulties: {nodes:[{difficulties:"łatwe"},{difficulties:"średnie"},{difficulties:"trudne"}]},
+        allStrapiTypes: {nodes:[{types:"obiad"},{types:"śniadania"},{types:"kolacja"}, {types:"deser"}, {types:"napój"}]}
+    }
 })
 
 const toggleInputs = (checkboxes)=>{
@@ -31,29 +28,38 @@ const checkInputsStatus = (checkboxes)=>{
 
 describe("Filter bar component", ()=>{
 it("renders filter bar correctly", ()=>{
+    const filterbarData =  {
+        allStrapiDiets: {nodes:[{diets: "wegańska"}, {diets:"wege"}, {diets:"bez-laktozy"}, {diets: "zbilansowana"}]},
+        allStrapiDifficulties: {nodes:[{difficulties:"łatwe"},{difficulties:"średnie"},{difficulties:"trudne"}]},
+        allStrapiTypes: {nodes:[{types:"obiad"},{types:"śniadania"},{types:"kolacja"}, {types:"deser"}, {types:"napój"}]}
+    }
     render (
         <TestWrapper>
-            <FilterBar isOpen={true}/>
+            <FilterBar data={filterbarData} isOpen={true}/>
         </TestWrapper>
     )
-console.log(query)
 const filterBarElement = screen.getByTestId('filterbar')
     expect(filterBarElement).toBeInTheDocument()
     expect(filterBarElement).toBeVisible()
 
 
 });
-// it("clears the filters", ()=>{
-//     render (
-//         <TestWrapper>
-//             <FilterBar isOpen={true}/>
-//         </TestWrapper>
-//     )
-// const clearButtonElement = screen.getByRole("button", {name: /Wyczyść/i})
-//     const checkboxElements = screen.getAllByTestId("checkbox-input")
-//     toggleInputs(checkboxElements)
-//     fireEvent.click(clearButtonElement)
-//     checkInputsStatus(checkboxElements)
-// })
+it("clears the filters", ()=>{
+    const filterbarData =  {
+        allStrapiDiets: {nodes:[{diets: "wegańska"}, {diets:"wege"}, {diets:"bez-laktozy"}, {diets: "zbilansowana"}]},
+        allStrapiDifficulties: {nodes:[{difficulties:"łatwe"},{difficulties:"średnie"},{difficulties:"trudne"}]},
+        allStrapiTypes: {nodes:[{types:"obiad"},{types:"śniadania"},{types:"kolacja"}, {types:"deser"}, {types:"napój"}]}
+    }
+    render (
+        <TestWrapper>
+            <FilterBar data={filterbarData} isOpen={true}/>
+        </TestWrapper>
+    )
+const clearButtonElement = screen.getByRole("button", {name: /Wyczyść/i})
+    const checkboxElements = screen.getAllByTestId("checkbox-input")
+    toggleInputs(checkboxElements)
+    fireEvent.click(clearButtonElement)
+    checkInputsStatus(checkboxElements)
+})
 
 })
